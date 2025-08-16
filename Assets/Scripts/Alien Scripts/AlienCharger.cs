0669fae966a4bc4a91cdb88ded6543744c2ff545
EthanB976 <136556095+EthanB976@users.noreply.github.com>
2025-08-16 20:56:57 +1000

    Added two new alien enemies, the charger and grander

---
+++ Assets/Scripts/Alien Scripts/AlienCharger.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class AlienCharger : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask detectionLayer;
+    [SerializeField] private float targetDetection = 10f;
+    [SerializeField] private float chargeRange = 9f;
+    [SerializeField] private float chargeTimer = 1f;
+    [SerializeField] private float chargeDuration = 5f;
+
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Rigidbody2D rb2D;
+    [SerializeField] private Vector2 chargeDirection;
+    [SerializeField] public bool isCharging;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] public bool isPreparingCharging = false;
+
+    [SerializeField] private AlienBase alienBase;
+    [SerializeField] private GameObject warning;
+
+    private void Start()
+    {
+        alienBase = GetComponent<AlienBase>();
+        warning.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (alienBase.stunned)
+        {
+            return;
+        }
+
+        DetectPlayer();
+
+        if (playerTransform != null)
+        {
+            float distance = Vector2.Distance(transform.position, playerTransform.position);
+
+            if (distance < chargeRange)
+            {
+                Charge();
+            }
+            else
+            {
+                MoveTowardsPlayer();
+            }
+
+        }
+
+    }
+
+    private void DetectPlayer()
+    {
+        Collider2D hitColliders = Physics2D.OverlapCircle(transform.position, targetDetection, detectionLayer);
+
+        if (hitColliders != null)
+        {
+            playerTransform = hitColliders.transform;
+        }
+        else
+        {
+            playerTransform = null;
+        }
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        Vector2 direction = (playerTransform.position - transform.position).normalized;
+        rb2D.linearVelocity = direction * speed;
+    }
+
+    private void Charge()
+    {
+        if (isCharging || isPreparingCharging)
+        {
+            return;
+        }
+
+        StartCoroutine(ChargingTime());
+    }
+
+    IEnumerator ChargingTime()
+    {
+        isPreparingCharging = true;
+        warning.SetActive(true);
+
+        rb2D.linearVelocity = Vector2.zero;
+
+        yield return new WaitForSeconds(chargeTimer);
+
+        chargeDirection = (playerTransform.position - transform.position).normalized;
+
+        isPreparingCharging = false;
+        warning.SetActive(false);
+        isCharging = true;
+
+        rb2D.linearVelocity = chargeDirection * speed * 5f;
+
+        yield return new WaitForSeconds(chargeDuration);
+
+        rb2D.linearVelocity = Vector2.zero;
+        isCharging = false;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, targetDetection);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position, chargeRange);
+
+
+    }
+
+}
---
+++ Assets/Scripts/Alien Scripts/AlienWanderCharger.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+public class AlienWanderCharger : MonoBehaviour
+{
+    [SerializeField] private float wanderRange = 10f;
+    [SerializeField] private float wanderTimer = 10f;
+    [SerializeField] private Vector2 wanderTarget;
+    [SerializeField] private bool isWaiting = false;
+    [SerializeField] private bool hasTarget = false;
+    [SerializeField] private bool stop = false;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed = 5f;
+
+    [SerializeField] private AlienBase alienBase;
+    [SerializeField] private AlienCharger alienCharger;
+
+    private void Start()
+    {
+        alienBase = GetComponent<AlienBase>();
+    }
+    private void Update()
+    {
+        if (alienBase.stunned)
+        {
+            return;
+        }
+
+        if (alienCharger.isCharging || alienCharger.isPreparingCharging)
+        {
+            return;
+        }
+
+        Wander();
+
+    }
+
+    public void Wander()
+    {
+        if (isWaiting || stop)
+        {
+            return;
+        }
+
+        if (!hasTarget)
+        {
+            Vector2 randomTarget = Random.insideUnitCircle * wanderRange;
+            wanderTarget = (Vector2)transform.position + randomTarget;
+            hasTarget = true;
+        }
+
+        Vector2 direction = (wanderTarget - (Vector2)transform.position).normalized;
+        rb.linearVelocity = direction * speed;
+
+        if (Vector2.Distance(transform.position, wanderTarget) < 0.5f && hasTarget)
+        {
+            hasTarget = false;
+            stop = true;
+            StartCoroutine(WanderStop());
+        }
+
+
+    }
+
+    public IEnumerator WanderStop()
+    {
+        isWaiting = true;
+        rb.linearVelocity = Vector2.zero;
+        yield return new WaitForSeconds(2);
+        isWaiting = false;
+        stop = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, wanderRange);
+    }
+
+}
