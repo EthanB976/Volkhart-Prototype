a5e2eb23b312f8608991bd13c7b97c137a92448c
EthanB976 <136556095+EthanB976@users.noreply.github.com>
2025-08-16 22:55:04 +1000

    failsafe added for puzzle

--- Assets/Scripts/Puzzle/WeightObject.cs
+++ Assets/Scripts/Puzzle/WeightObject.cs
@@ -3,4 +3,22 @@ using UnityEngine;
 public class WeightObject : MonoBehaviour
 {
     public int weight = 0;
+
+    [HideInInspector] public Vector3 startPosition;
+
+    private void Awake()
+    {
+        startPosition = transform.position; // store starting location
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = startPosition;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
+    }
 }
