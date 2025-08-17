49ee789c1b8c5106362d37b1cae397d86b2e645f
EthanB976 <136556095+EthanB976@users.noreply.github.com>
2025-08-17 20:07:42 +1000

    finished all scenes

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -6,6 +6,11 @@ public class Restart : MonoBehaviour
 {
     public void RestartLevel()
     {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReturnToMenu()
+    {
         SceneManager.LoadScene(0);
     }
 }
