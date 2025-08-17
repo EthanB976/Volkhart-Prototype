68664691c29b32966f5ffbe5fe7a8668977a0943
HerbalsGerbals <1032054@student.sae.edu.au>
2025-08-17 15:36:19 +1000

    Added Tutorial Room Advice

--- Assets/Scripts/Enviromental Scripts/TutorialAdviceScript.cs
+++ Assets/Scripts/Enviromental Scripts/TutorialAdviceScript.cs
@@ -1,16 +1,33 @@
+using TMPro;
 using UnityEngine;
 
 public class TutorialAdviceScript : MonoBehaviour
 {
+    public string text;
+    public GameObject tutorialAdviceGameObject;
+    public TextMeshProUGUI tutorialAdviceText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            tutorialAdviceGameObject.gameObject.SetActive(true);
+            tutorialAdviceText.text = text;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            tutorialAdviceGameObject.gameObject.SetActive(false);
+        }
     }
 }
